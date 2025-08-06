b75d60b53c6be656d475c77fb9a683ade5106f87
Maher35a <Maher35a@gmail.com>
2025-08-07 01:53:06 +0300

    add notes cubit

--- lib/cubits/addnote_cubit.dart
+++ lib/cubits/addnote_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+
+import '../models/note-model.dart';
 
 part 'addnote_state.dart';
 
 class AddnoteCubit extends Cubit<AddnoteState> {
   AddnoteCubit() : super(AddnoteInitial());
+   addnote(NoteModel note)async{
+     emit(AddnoteLoading());
+    try{ var notesbox = Hive.box<NoteModel>('notes');
+      emit(AddnoteSuccess());
+      await notesbox.add(note);}
+        on HiveError catch(e){
+          emit(AddnoteFailure(e.toString()));
+        }
+  }
 }
--- lib/cubits/addnote_state.dart
+++ lib/cubits/addnote_state.dart
@@ -2,7 +2,6 @@ part of 'addnote_cubit.dart';
 
 @immutable
 sealed class AddnoteState {}
-
 final class AddnoteInitial extends AddnoteState {}
 final class AddnoteLoading extends AddnoteState {}
 final class AddnoteSuccess extends AddnoteState {}
